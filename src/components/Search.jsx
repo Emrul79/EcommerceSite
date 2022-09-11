cf16282630671806681e162ca6a386d8752f4f13
Emrul Hassan Biplob <hemrul494@gmail.com>
2022-09-11 23:44:50 +0600

    serchbar-modified

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,15 +6,32 @@ import { useState } from 'react'
 import { fetchDeta, exerciseOptions } from '../utilities/Fetchdeta'
 
 const Search = () => {
-    const [search,setsearch]=useState('')
+    const [search, setSearch] = useState('');
+    const [exercise, setexercise] = useState([]);
 
-
-    const handleSearch=async()=>{
-        if(search){
-            const exercisesDeta = await fetchDeta('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
-            console.log(exercisesDeta);
+    console.log(search);
+    const handleSearch =async  (e) => {
+        
+        console.log('hello world');
+        if (search) {
+            const exercisesDeta = await  fetchDeta('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
+            const searchexercise =await exercisesDeta.filter(
+                (exercise) => exercise.name.toLowerCase().includes(search)
+                    ||
+                    exercise.target.toLowerCase().includes(search)
+                    ||
+                    exercise.equipment.toLowerCase().includes(search)
+                    ||
+                    exercise.bodypart.toLowerCase().includes(search)
+                
+            );
+            window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+            setSearch('');
+            setexercise(searchexercise);
         }
-    } 
+        
+    
+    }
     return (
         <Stack
             alignItems="center"
@@ -30,7 +47,7 @@ const Search = () => {
                 textAlign="center"
             >
                 Awesome Exercise Your <br />
-                Should Know
+                Should Know moreeee
             </Typography>
             <Box
                 position="relative"
@@ -39,37 +56,37 @@ const Search = () => {
                 <TextField
                     sx={{
                         input: {
-                            
+
                             fontweight: "700",
                             border: "none",
                             borderRedius: "4px"
                         },
-                        width: { lg: "700px",md:"450px" ,xs: "300px"  },
-                        backgroundColor:"#ddd",
-                        borderRedius:"40px"
+                        width: { lg: "700px", md: "450px", xs: "300px" },
+                        backgroundColor: "#ddd",
+                        borderRedius: "40px"
                     }}
 
 
                     height="76px"
                     value={search}
-                    onChange={(e) => setsearch(e.target.value.toLowerCase())}
+                    onChange={(e) => setSearch(e.target.value.toLowerCase())}
                     placeholder="Serch Exercixes"
                     type="text"
                 />
 
                 <Button className='search-btn'
-                sx={{
-                    bgcolor:"#ff2625",
-                    color:"#fff",
-                    textTransform:"none",
-                    width:{lg:"175px",xs:"80px"},
-                    fontSize:{lg:"20px", xs:"14px"},
-                    height:"56px",
-                    transition:"0.5s all"
-            
-                }}
+                    sx={{
+                        bgcolor: "#ff2625",
+                        color: "#fff",
+                        textTransform: "none",
+                        width: { lg: "175px", xs: "80px" },
+                        fontSize: { lg: "20px", xs: "14px" },
+                        height: "56px",
+                        transition: "0.5s all"
 
-                onClick={handleSearch}
+                    }}
+
+                    onClick={handleSearch}
                 >
                     Search
                 </Button>
--- src/utilities/Fetchdeta.jsx
+++ src/utilities/Fetchdeta.jsx
@@ -4,11 +4,11 @@ export const exerciseOptions = {
     method: 'GET',
 
     headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
         'X-RapidAPI-Key': "25b2d26778msh2a0f2be430382b2p13db8ejsn345f9ec75854",
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
+    },
 
-}
+};
 
 
 
